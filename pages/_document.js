99dc456a798a592b2309e03e7c1fff92daf51085
Madhav Dave <davemadhav2007@gmail.com>
2025-07-07 13:45:13 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,7 @@ export default function Document() {
         <link href="https://fonts.googleapis.com/css2?family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Quicksand:wght@300..700&family=Righteous&family=Roboto:ital,wght@0,100..900;1,100..900&family=Signika:wght@300..700&family=The+Nautigal:wght@400;700&display=swap" rel="stylesheet"></link>
       </Head>
 
-      <body className="bg-slate-100" >
+      <body>
         <Sidebar></Sidebar>
         <Main />
         <NextScript />

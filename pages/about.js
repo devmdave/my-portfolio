e0366c5e3e13995ead5c85765ebd2772d9dbec52
devmdave <davemadhav2007@gmail.com>
2025-12-05 00:48:05 +0530

    added education icon

--- pages/about.js
+++ pages/about.js
@@ -211,10 +211,20 @@ export default function AboutPage() {
 
           {/* 3. Education Section */}
           <section className="animate-fade-in-up">
-            <div className="mx-auto glass-card shadow-xl rounded-2xl max-md:w-[80vw] mb-30 mt-10">
+            <div className="mx-auto glass-card shadow-xl rounded-2xl max-md:w-[80vw] mb-30 mt-5">
               <div className="rounded-xl p-6 mb-4">
-                <h3 className="text-2xl font-bold text-slate-700 mb-4 text-left">
-                  🎓 Education
+                <h3 className="text-2xl tracking-wider flex flex-row font-bold text-slate-700 mb-4 text-left">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    class="size-8 my-auto mr-2  text-slate-700"
+                  >
+                    <path d="M11.7 2.805a.75.75 0 0 1 .6 0A60.65 60.65 0 0 1 22.83 8.72a.75.75 0 0 1-.231 1.337 49.948 49.948 0 0 0-9.902 3.912l-.003.002c-.114.06-.227.119-.34.18a.75.75 0 0 1-.707 0A50.88 50.88 0 0 0 7.5 12.173v-.224c0-.131.067-.248.172-.311a54.615 54.615 0 0 1 4.653-2.52.75.75 0 0 0-.65-1.352 56.123 56.123 0 0 0-4.78 2.589 1.858 1.858 0 0 0-.859 1.228 49.803 49.803 0 0 0-4.634-1.527.75.75 0 0 1-.231-1.337A60.653 60.653 0 0 1 11.7 2.805Z" />
+                    <path d="M13.06 15.473a48.45 48.45 0 0 1 7.666-3.282c.134 1.414.22 2.843.255 4.284a.75.75 0 0 1-.46.711 47.87 47.87 0 0 0-8.105 4.342.75.75 0 0 1-.832 0 47.87 47.87 0 0 0-8.104-4.342.75.75 0 0 1-.461-.71c.035-1.442.121-2.87.255-4.286.921.304 1.83.634 2.726.99v1.27a1.5 1.5 0 0 0-.14 2.508c-.09.38-.222.753-.397 1.11.452.213.901.434 1.346.66a6.727 6.727 0 0 0 .551-1.607 1.5 1.5 0 0 0 .14-2.67v-.645a48.549 48.549 0 0 1 3.44 1.667 2.25 2.25 0 0 0 2.12 0Z" />
+                    <path d="M4.462 19.462c.42-.419.753-.89 1-1.395.453.214.902.435 1.347.662a6.742 6.742 0 0 1-1.286 1.794.75.75 0 0 1-1.06-1.06Z" />
+                  </svg>
+                  EDUCATION
                 </h3>
 
                 <div className="text-lg font-semibold text-slate-800 text-left">
@@ -232,34 +242,34 @@ export default function AboutPage() {
                   </li>
                   <li>Clubs: Coding Club, Tech Society</li>
                 </ul>
-                  <div className="grid md:grid-cols-2 gap-4">
-                    <div className="bg-white/50 rounded-xl p-4 border border-white/60">
-                      <h4 className="font-semibold text-slate-700 mb-1">
-                        Specialization
-                      </h4>
-                      <p className="text-slate-600 text-sm">
-                        Artificial Intelligence & Data Science
-                      </p>
-                    </div>
-                    <div className="bg-white/50 rounded-xl p-4 border border-white/60">
-                      <h4 className="font-semibold text-slate-700 mb-1">
-                        Achievements
-                      </h4>
-                      <p className="text-slate-600 text-sm">
-                        Dean's List, Best Capstone Project Award
-                      </p>
-                    </div>
+                <div className="grid md:grid-cols-2 gap-4">
+                  <div className="bg-white/50 rounded-xl p-4 border border-white/60">
+                    <h4 className="font-semibold text-slate-700 mb-1">
+                      Specialization
+                    </h4>
+                    <p className="text-slate-600 text-sm">
+                      Artificial Intelligence & Data Science
+                    </p>
                   </div>
+                  <div className="bg-white/50 rounded-xl p-4 border border-white/60">
+                    <h4 className="font-semibold text-slate-700 mb-1">
+                      Achievements
+                    </h4>
+                    <p className="text-slate-600 text-sm">
+                      Dean's List, Best Capstone Project Award
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </section>
 
           {/* 4. Experience/Roles Section */}
           <section className="w-[90vw] glass-card rounded-xl backdrop-blur-sm p-8 mx-auto bg-white">
-            <h2 className="text-3xl font-bold text-slate-800 mb-10 text-center animate-fade-in-up">
+            <h2 className="text-3xl font-bold text-slate-800 mb-10 text-left animate-fade-in-up">
               WORK EXPEREINCE
             </h2>
-            <div className="grid md:grid-cols-2 gap-6">
+            <div className="grid  md:grid-cols-2 gap-6">
               {/* Role 1 */}
               <div className="glass-card p-8 rounded-3xl animate-scale-in delay-100 group">
                 <div className="flex justify-between items-start mb-6">
